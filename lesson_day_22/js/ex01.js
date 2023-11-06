be159efb6c25fab770ad466143317dd9896faab6
Tornado-1 <tornado-1@Admin-1s-iMac.local>
2023-11-06 17:44:38 +0800

    add:lesson_day_22

---
+++ lesson_day_22/js/ex01.js
@@ -0,0 +1,11 @@
+var header = document.createElement("h1");
+header.innerHTML = 'Welcome to My Website'
+document.body.appendChild(header);
+
+var main = document.createElement("p");
+main.innerHTML = 'This is a small website created using HTML and JavaScript.'
+document.body.appendChild(main);
+
+var footer = document.createElement("p");
+footer.innerHTML = '&copy; 2023 Your Name';
+document.body.appendChild(footer);
