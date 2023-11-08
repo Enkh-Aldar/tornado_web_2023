4b79fc8be26fc8db97ff73c6e47c48f2cbb019bc
Tornado-1 <tornado-1@Admin-1s-iMac.local>
2023-11-08 17:31:19 +0800

    add:projects

---
+++ lesson_day_23/ex_01.js
@@ -0,0 +1,10 @@
+var title = document.createElement('div');
+title.setAttribute('class', 'my-class');
+title.style.backgroundColor = "yellow";
+title.innerText = 'Element 1';
+document.body.appendChild(title);
+var titleOne = document.createElement('div');
+titleOne.setAttribute('class', 'my-class');
+titleOne.style.backgroundColor = "orange";
+titleOne.innerText = 'Element 2';
+document.body.appendChild(titleOne);
