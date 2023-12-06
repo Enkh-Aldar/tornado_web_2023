c3cf6cbc7a7dc6c272583e022e54914cc94ff962
Tornado-1 <tornado-1@Admin-1s-iMac.local>
2023-12-06 17:00:09 +0800

    add:lesson_day_30_react

---
+++ lesson_day_30_react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Counter from "./components/Counter";
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Use State in ReactJS</h1>
+      <header className="App-header">
+        <Counter />
+      </header>
+    </div>
+  )
+}
+
+export default App;
