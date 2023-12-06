97a40904475f0310cd4f2d5b2f8cd901aa5a86f9
Tornado-1 <tornado-1@Admin-1s-iMac.local>
2023-12-06 16:59:03 +0800

    add:lesson_day_35_react

---
+++ lesson_day_35_react/src/pages/Team.jsx
@@ -0,0 +1,9 @@
+function Team(){
+    return(
+        <div className="p-6 max-4-xl mx-auto">
+            <h2 className="text-3xl font-bold text-gray-800">Team Page</h2>
+            <p className="mt-2 text-lg text-gray-600">Meet Our Team.</p>
+        </div>
+    )
+}
+export default Team;
